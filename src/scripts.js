4734b4e607088064bd943a907a0fff2a64b8c1a6
Ryan Bahan <ryan@ryanbahan.com>
2020-02-17 21:01:44 -0700

    Update nav

--- src/scripts.js
+++ src/scripts.js
@@ -10,13 +10,13 @@ import Cookbook from './cookbook';
 import DomUpdates from './domUpdates';
 
 // let favButton = document.querySelector('.view-favorites');
-let homeButton = document.querySelector('.home');
-let cardArea = document.querySelector('.all-cards');
+// let homeButton = document.querySelector('.home');
+// let cardArea = document.querySelector('.all-cards');
 let domUpdates = new DomUpdates();
 
-homeButton.addEventListener('click', cardButtonConditionals);
+// homeButton.addEventListener('click', cardButtonConditionals);
 // favButton.addEventListener('click', viewFavorites);
-cardArea.addEventListener('click', cardButtonConditionals);
+// cardArea.addEventListener('click', cardButtonConditionals);
 
 let userId = (Math.floor(Math.random() * 49) + 1);
 let user;

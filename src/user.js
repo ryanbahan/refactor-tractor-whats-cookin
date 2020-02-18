0e4d5f21833ac747faab94a30a5e883f6c7cd2af
Erik Raith <raithhtiar@gmail.com>
2020-02-18 12:21:36 -0700

    [refactor] Add load,save,updateFavorites() methods
    These methods on the user Class allow for favorites to be stored and
    updated in localStorage

--- src/user.js
+++ src/user.js
@@ -4,18 +4,26 @@ class User {
     this.name = name;
     this.pantry = pantry;
     this.favoriteRecipes = [];
-
   }
 
-  addToFavorites(recipe) {
-    if (!this.favoriteRecipes.includes(recipe)) {
-      this.favoriteRecipes.push(recipe)
+  loadFavorites() {
+    this.favoriteRecipes = JSON.parse(localStorage.getItem(this.id)) || [];
+  }
+
+  saveFavorites() {
+    localStorage.setItem(this.id, JSON.stringify(this.favoriteRecipes));
+  }
+
+  updateFavorites() {
+    if (this.favoriteRecipes.includes(event.target.id)) {
+      this.favoriteRecipes.splice(
+        this.favoriteRecipes.indexOf(event.target.id),
+        1
+      );
+    } else {
+      this.favoriteRecipes.push(event.target.id);
     }
-  }
-
-  removeFromFavorites(recipe) {
-    const i = this.favoriteRecipes.indexOf(recipe);
-    this.favoriteRecipes.splice(i, 1)
+    this.saveFavorites();
   }
 
   filterFavorites(tag) {
@@ -26,31 +34,39 @@ class User {
 
   findFavorites(strgToSrch) {
     return this.favoriteRecipes.filter(recipe => {
-      return recipe.name.includes(strgToSrch) ||
+      return (
+        recipe.name.includes(strgToSrch) ||
         recipe.ingredients.find(ingredient => {
-          return ingredient.name.includes(strgToSrch)
-        });
+          return ingredient.name.includes(strgToSrch);
+        })
+      );
     });
   }
 
   checkPantry(recipe) {
-    let missingIngredients = recipe.ingredients.reduce((missingItems, ingredient) => {
-      let pantryIngredientQnt = (this.pantry.find(x => x.ingredient === ingredient.id) || 0);
-      if (pantryIngredientQnt != 0) {
-        pantryIngredientQnt = pantryIngredientQnt.amount;
-      }
-      if (pantryIngredientQnt - ingredient.quantity.amount < 0) {
-        missingItems.push({
-          name: ingredient.name,
-          quantity: {
-            amount: ingredient.quantity.amount - pantryIngredientQnt,
-            unit: ingredient.quantity.unit
-          },
-          cost: (ingredient.quantity.amount - pantryIngredientQnt) * recipe.findCost(ingredient.id),
-        });
-      }
-      return missingItems;
-    }, [])
+    let missingIngredients = recipe.ingredients.reduce(
+      (missingItems, ingredient) => {
+        let pantryIngredientQnt =
+          this.pantry.find(x => x.ingredient === ingredient.id) || 0;
+        if (pantryIngredientQnt != 0) {
+          pantryIngredientQnt = pantryIngredientQnt.amount;
+        }
+        if (pantryIngredientQnt - ingredient.quantity.amount < 0) {
+          missingItems.push({
+            name: ingredient.name,
+            quantity: {
+              amount: ingredient.quantity.amount - pantryIngredientQnt,
+              unit: ingredient.quantity.unit
+            },
+            cost:
+              (ingredient.quantity.amount - pantryIngredientQnt) *
+              recipe.findCost(ingredient.id)
+          });
+        }
+        return missingItems;
+      },
+      []
+    );
     return missingIngredients;
   }
 }

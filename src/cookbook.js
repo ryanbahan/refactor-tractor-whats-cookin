bd365b149ce1300f94aa8ff15708c23faa23607f
Erik Raith <raithhtiar@gmail.com>
2020-02-18 15:41:49 -0700

    [feat] Add Cookbook properties savedRecipes and Fav

--- src/cookbook.js
+++ src/cookbook.js
@@ -1,16 +1,36 @@
 class Cookbook {
-  constructor(data) {
-    this.recipes = data;
+  constructor(userID) {
+    this.id = userID;
+    let storedValue = JSON.parse(localStorage.getItem(2)) || {
+      favorites: [],
+      savedRecipes: []
+    };
+    this.favoriteRecipes = storedValue.favorites;
+    this.savedRecipes = storedValue.savedRecipes;
+  }
+  
+  save() {
+    localStorage.setItem(
+      this.id,
+      JSON.stringify({
+        favorites: this.favoriteRecipes,
+        savedRecipes: this.savedRecipes
+      })
+    );
   }
 
   findRecipe(searchText) {
     return this.recipes.filter(recipe => {
       return recipe.ingredients.find(ingredient => {
-        return (ingredient.name.includes(searchText)) ||
-        (recipe.name.includes(searchText))
+        return (
+          ingredient.name.includes(searchText) ||
+          recipe.name.includes(searchText)
+        );
       });
-    })
+    });
   }
+  
+
 }
 
 export default Cookbook;

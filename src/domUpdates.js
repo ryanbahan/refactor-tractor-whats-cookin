aaa372827a96b7d7671d7234932dfb816ca6a7fc
Ryan Bahan <ryan@ryanbahan.com>
2020-02-18 15:29:07 -0700

    Add html and styling for recipes view

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -18,27 +18,28 @@ class DomUpdates {
       }
       recipes.forEach(recipe => {
         let isFavorite = '';
+
         if(favorites.includes(`${recipe.id}`)){
           isFavorite = 'favorite-active';
         } else {
           isFavorite = 'favorite';
         }
+
         target.insertAdjacentHTML('afterbegin', `<div id='${recipe.id}'
         class='card'>
+
               <img id='${recipe.id}' tabindex='0' class='card-picture'
               src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
-        <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
-        <header id='${recipe.id}' class='card-header'>
-          <label for='add-button' class='hidden'>Click to add recipe</label>
+              <div class="card-title-info">
+        <p id='${recipe.id}' class='recipe-name'>${recipe.name}</p>
+
+          <div class="card-button-container">
           <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
-            <img id='${recipe.id} favorite' class='add'
-            src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
-            recipes to cook'>
           </button>
-          <label for='favorite-button' class='hidden'>Click to favorite recipe
-          </label>
-          <button id='${recipe.id}' aria-label='favorite-button' class='favorite ${isFavorite} favorite${recipe.id} card-button'></button>
-        </header>
+          <button id='${recipe.id}' aria-label='favorite-button' class='favorite ${isFavorite} favorite${recipe.id} card-button'>
+          </button>
+          </div>
+          </div>
         </div>`)
       })
     };

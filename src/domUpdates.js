c3015577d53a6c4c428605785754c371c1ad989b
Ryan Bahan <ryan@ryanbahan.com>
2020-02-17 20:39:00 -0700

    Update css styling

--- src/domUpdates.js
+++ src/domUpdates.js
@@ -25,20 +25,20 @@ class DomUpdates {
         }
         target.insertAdjacentHTML('afterbegin', `<div id='${recipe.id}'
         class='card'>
-            <header id='${recipe.id}' class='card-header'>
-              <label for='add-button' class='hidden'>Click to add recipe</label>
-              <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
-                <img id='${recipe.id} favorite' class='add'
-                src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
-                recipes to cook'>
-              </button>
-              <label for='favorite-button' class='hidden'>Click to favorite recipe
-              </label>
-              <button id='${recipe.id}' aria-label='favorite-button' class='favorite ${isFavorite} favorite${recipe.id} card-button'></button>
-            </header>
-              <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
               <img id='${recipe.id}' tabindex='0' class='card-picture'
               src='${recipe.image}' alt='click to view recipe for ${recipe.name}'>
+        <span id='${recipe.id}' class='recipe-name'>${recipe.name}</span>
+        <header id='${recipe.id}' class='card-header'>
+          <label for='add-button' class='hidden'>Click to add recipe</label>
+          <button id='${recipe.id}' aria-label='add-button' class='add-button card-button'>
+            <img id='${recipe.id} favorite' class='add'
+            src='https://image.flaticon.com/icons/svg/32/32339.svg' alt='Add to
+            recipes to cook'>
+          </button>
+          <label for='favorite-button' class='hidden'>Click to favorite recipe
+          </label>
+          <button id='${recipe.id}' aria-label='favorite-button' class='favorite ${isFavorite} favorite${recipe.id} card-button'></button>
+        </header>
         </div>`)
       })
     };

c3b942cb3141a80199ce4419cd231ce0dd4d8c47
Erik Raith <raithhtiar@gmail.com>
2020-02-25 17:41:07 -0700

    [syntax] Spaces removed

--- test/user-test.js
+++ test/user-test.js
@@ -3,29 +3,30 @@ const chai = require("chai"),
 chai.use(spies);
 import { expect } from "chai";
 
-import Recipe from '../src/recipe.js';
-import recipeData from '../src/data/recipes.js';
-import ingredientsData from '../src/data/ingredients.js';
-import Cookbook from '../src/cookbook.js';
-import Pantry from '../src/pantry.js'
-import User from '../src/user.js'
-let user1, recipe,pantry;
+import Recipe from "../src/recipe.js";
+import recipeData from "../src/data/recipes.js";
+import ingredientsData from "../src/data/ingredients.js";
+import Cookbook from "../src/cookbook.js";
+import Pantry from "../src/pantry.js";
+import User from "../src/user.js";
+let user1, recipe, pantry;
 global.localStorage = {};
 
-describe('User', () => {
-
+describe("User", () => {
   beforeEach(() => {
-    pantry = [{"ingredient":11477,"amount":4},{"ingredient":11297,"amount":4}]
-    recipe = new Recipe(recipeData[0], ingredientsData)
-    user1 = new User(1, 'Boba', pantry);
+    pantry = [
+      { ingredient: 11477, amount: 4 },
+      { ingredient: 11297, amount: 4 }
+    ];
+    recipe = new Recipe(recipeData[0], ingredientsData);
+    user1 = new User(1, "Boba", pantry);
   });
 
-  it('Should have a cookbook', () => {
-    expect(user1.cookbook).to.be.a('object');
+  it("Should have a cookbook", () => {
+    expect(user1.cookbook).to.be.a("object");
   });
 
-  it('Should have a pantry', () => {
-    expect(user1.pantry).to.be.a('object');
-    
+  it("Should have a pantry", () => {
+    expect(user1.pantry).to.be.a("object");
   });
 });
